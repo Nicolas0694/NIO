d68b77f5ad33080ff6369a0400cc786275737bc2
IvoCostaCunha <ivocostacunha@gmail.com>
2018-11-22 17:25:19 +0100

    Ajout de constructeurs

--- js/framework/Map.js
+++ js/framework/Map.js
@@ -1,5 +1,10 @@
 /* Cette classe est l'ensemble des objets constituant la map */
 class Map 
 {
-
+    constructor(startPJY,list_Platformes,list_PNGs,PJ){
+        this.startPJ = startPJ;
+        this.list_Platformes = list_Platformes;
+        this.list_PNGs = list_PNGs;
+        this.PJ = PJ;
+    }
 }
--- js/framework/Platform.js
+++ js/framework/Platform.js
@@ -2,18 +2,21 @@
 class Platform {
 
     /* constructeur de la platforme, mx, et my booleans qui definissent le type de mouvement et v la vitesse */
-    constructor (x,y,mx,my,v){
+    /* taille varie de 1 a 3 , 2 et 3 etant 2* et 3* la taille de la platfome de base en 16 pixels de long */
+    constructor (x,y,mx,my,v,taille){
         this.x = x;
         this.y = y;
         this.mx = mx;
         this.my = my;
-        this.v = v;   
+        this.v = v;
+        this.taille = taille;   
     }
 
     /* constructeur d'une plaforme statique */
-    constructor (x,y){
+    constructor (x,y,taille){
         this.x = x;
         this.y = y;
+        this.taille = taille; 
     }
 
     animation(){
